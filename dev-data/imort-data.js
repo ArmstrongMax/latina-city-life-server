a3212b1a77120e420f381e06167fc79afff815ca
Maksim Mikhaylov <mr.bellic@yandex.ru>
2022-01-14 00:01:43 +0300

    Edited parties model

--- dev-data/imort-data.js
+++ dev-data/imort-data.js
@@ -1,19 +1,13 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-dotenv.config({ path: './config.env' })
+dotenv.config({ path: '../config.env' })
 const Party = require('../models/partiesModel')
 const fs = require ('fs')
 
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('DB connection successful')
+mongoose.connect(DB).then(() => { console.log('Database connection successful')
 })
 
 const parties = JSON.parse(fs.readFileSync(`${__dirname}/parties.json`, 'utf8'))
