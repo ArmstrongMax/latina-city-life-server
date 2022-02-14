bb0be0965f28994e0242541e3b9d94ecdb40c5a8
Maksim Mikhaylov <mr.bellic@yandex.ru>
2022-02-15 02:05:14 +0300

    setting up for deploy

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const compression = require('compression')
 
 
 const app = express();
-app.use(cors({origin: ['http://127.0.0.1:3000', 'https://latina-city-life-client.herokuapp.com'], credentials:true}))
+app.use(cors({origin: 'https://latina-city-life-client.herokuapp.com', credentials:true}))
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
